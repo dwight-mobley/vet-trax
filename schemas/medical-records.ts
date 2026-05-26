7b24949c0a963d7deccf9c9bd16bd732f97a0957
dmobley0608 <dmobley0608@gmail.com>
2026-05-25 20:53:23 -0400

    feat(schema): add medical record schema

---
+++ schemas/medical-records.ts
@@ -0,0 +1,17 @@
+import {z} from "zod";
+import { publicMedicalRecordsInsertSchema} from "./database"
+
+export type MedicalRecord = z.infer<typeof publicMedicalRecordsInsertSchema>
+
+//Creation Schema and Type
+export const MedicalRecordCreateFormSchema = publicMedicalRecordsInsertSchema.omit({id:true}).extend({
+    record_date: z.string().nullable().transform((val) => val === "" ? null : val),
+    weight: z.number().nullable().transform((val) => val === null || val <= 0 ? null : val),
+    height: z.float32().nullable().transform((val) => val === null || val <= 0 ? null : val),
+})
+
+export type MedicalRecordCreateForm = z.infer<typeof MedicalRecordCreateFormSchema>
+
+//Update Schema and Type
+export const MedicalRecordUpdateFormSchema = MedicalRecordCreateFormSchema.partial();
+export type MedicalRecordUpdateForm = z.infer<typeof MedicalRecordUpdateFormSchema>
